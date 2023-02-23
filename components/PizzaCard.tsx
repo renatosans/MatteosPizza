a9e686e798237036d2fed48b7600098a33d2df2c
renatosans <renato@datacopy.com.br>
2023-02-23 11:55:57 -0300

    Ajustes

--- components/PizzaCard.tsx
+++ components/PizzaCard.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import styles from "../styles/PizzaCard.module.css";
 
 
-const PizzaCard = ({pizza}) => {
+const PizzaCard = ({pizza}: any) => {
   return (
     <div className={styles.container}>
-      <Image src={pizza.image} width="500" height="500" />
+      <Image src={pizza.image} alt={pizza.name} width={250} height={250} />
       <h1 className={styles.title}>{pizza.name}</h1>
       <span className={styles.price}>{pizza.id}</span>
       <p className={styles.desc}>{pizza.location.name}</p>
