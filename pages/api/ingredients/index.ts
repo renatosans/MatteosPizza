7d151d35f44a45d45fb71329fe52afc54af54e44
renatosans <renato@datacopy.com.br>
2023-02-25 09:42:13 -0300

    Inclusão de funcionalidade para upload de foto

--- pages/api/ingredients/index.ts
+++ pages/api/ingredients/index.ts
@@ -1,4 +1,7 @@
+import fs from 'fs'
+import path from 'path'
 import { prisma } from '../../../utils/connection'
+import { ingredientType } from '../../../utils/types'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 
@@ -13,8 +16,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	}
 }
 
+// TODO : Fix image upload
 const saveIngredient = async (req: NextApiRequest, res: NextApiResponse) => {
-    prisma.ingredients.create({ data: req.body })
+	const { ingredient_id, ingredient_name, flag, supplier, imageData } = req.body;
+
+	const dir = '/img/produit/ingredients/';
+	const filename = "ingredient999.jpg";
+	// path.resolve('./public', dir, filename)
+	const buffer = Buffer.from(imageData, 'base64');
+	fs.open(filename, "w", (err, fd) => {
+		fs.write(fd, buffer, 0, buffer.length, (err, writtenBytes, buffer) => {
+			console.log(`Wrote ${writtenBytes} bytes to file`);
+		});
+	}); 
+
+	const img: string = dir + filename;
+	const newIngredient: ingredientType = { ingredient_id, ingredient_name, flag, img, supplier };
+
+    prisma.ingredients.create({ data: newIngredient })
     .then((result) => res.send(result))
 	.catch((error) => res.send("Error: " + error.message))
 }
