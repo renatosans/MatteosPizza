f1be3d1d3084f30e2e109f6b828ec110bce9fd48
renatosans <renato@datacopy.com.br>
2023-02-27 06:11:46 -0300

    Criado frontend para o endpoint  /api/ingredients

--- components/IngredientForm.tsx
+++ components/IngredientForm.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/Ingredient.module.css"
 import { notification } from '../utils/notification'
-import { useState, useEffect, FormEvent } from 'react'
+import { useState, useEffect, FormEvent, ChangeEvent } from 'react'
 import toast, { Toaster, ToastOptions } from "react-hot-toast";
 
 
@@ -12,6 +12,11 @@ export const IngredientForm = ({dialogRef}: any) => {
 		supplier: "",
 	})
 
+    const [image, setImage] = useState({
+        imageFormat: "",
+        imageData: "",
+    });
+
     const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault();
 
@@ -21,28 +26,48 @@ export const IngredientForm = ({dialogRef}: any) => {
 		}
 
         try {
+            const payload = {...ingredient, ...image};
+
             await fetch(`/api/ingredients`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', },
-                body: JSON.stringify(ingredient),
+                body: JSON.stringify(payload),
             })
         } catch (error) {
             // ...
+            // ...
         }
 
-        // ...
+		toast.success('Produto salvo com sucesso', notification.options as ToastOptions);
+		dialogRef.toggle();
     }
+
+	const onChange = (e: any) => {
+		if (e.target.type === 'file') {
+			const file = e.target.files[0];
+			// Reads the file using the FileReader API
+			const reader = new FileReader();
+			reader.onloadend = () => {
+				const fileData = reader.result.split(';base64,');
+				let formato = fileData[0].replace('data:', '') + ';base64';
+                setImage({imageFormat: formato, imageData: fileData[1]});
+			}
+			reader.readAsDataURL(file);
+		}
+
+        setIngredient({...ingredient, [e.target.name]: e.target.value, });
+	};
 
     return (
     <>
         <Toaster />
         <form onSubmit={handleSubmit} className={styles.form} >
-            <input type="text" name="ingredient_name" value={ingredient.ingredient_name}/>
-            <input type="supplier" name="supplier" value={ingredient.supplier}/>
+            <input type="text" className={styles.input} name="ingredient_name" value={ingredient.ingredient_name} onChange={onChange} />
+            <input type="text" className={styles.input} name="supplier" value={ingredient.supplier} onChange={onChange} />
 
             <div className={styles.fileDialog}>
-                <input type="file" name="foto" accept=".gif,.jpg,.jpeg,.png" />
-                <img src="logo.png"></img>
+                <input type="file" name="foto" accept=".gif,.jpg,.jpeg,.png" onChange={onChange} />
+                <img style={{'width':'100%'}} src={"data:" + image.imageFormat + ", " + image.imageData} alt={ingredient.ingredient_name}></img>
             </div>
 
             <button type="submit" className={styles.button}>Salvar</button>
